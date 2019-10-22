71a1ac3848b615fd2f4b6f847615a999f0739930
Rafał Krupiński <rafal.krupinski@tooploox.com>
2019-10-22 15:49:29 +0200

    Ignore dist

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createResource = function () {
-    throw new Error("Not implemented");
-};
-//# sourceMappingURL=index.js.map
